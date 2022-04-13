4893e7800a05249726da3f1231cc94a4167208fb
gk <gk@uni-bonn.de>
2022-04-13 18:02:43 +0200

    Code cleanup

--- R/CroptimizeR/SimplaceFunction.R
+++ R/CroptimizeR/SimplaceFunction.R
@@ -1,56 +1,58 @@
 #' Define the optimisation function according to the CroptimizeR specification
 
-SimplaceFunction <- function( param_values=NULL, 
-                              sit_names=NULL, 
-                              model_options,
-                              sit_var_dates_mask=NULL, ...) {
-  
-  # Create empty result, set error to true
-  results <-  list(sim_list=list(), error=TRUE)
+SimplaceFunction <- function(param_values=NULL,
+                             sit_names=NULL,
+                             model_options,
+                             sit_var_dates_mask=NULL, ...) {
 
-  # Perform the simulations, don't break if Simplace breaks but return error=TRUE 
+  # Create empty result, set error to true
+  results <-  list(sim_list = list(), error = TRUE)
+
+  # Perform simulations, don't break if Simplace breaks but return error=TRUE
   tryCatch({
-    
+
     # fetch parameters from CroptimizeR and add the values to the parameter list
-    # If required some (pre)processing or check of parameters could be done here 
+    # If required some (pre)processing or check of parameters could be done here
     # if the Simplace solution can't handle some parameters directly
     param <- list()
     for (iparam in names(param_values)) {
-      param[[iparam]]=param_values[iparam]
+      param[[iparam]] <- param_values[iparam]
     }
-    
+
     # Create and run simulation for each situation
-    for(situation in model_options$Situations)
-    {
+    for (situation in model_options$Situations) {
       # Add the situation to the parameters
-      param$projectid <- situation  
-      param[[model_options$SituationVariable]] <- situation 
+      param$projectid <- situation
+      param[[model_options$SituationVariable]] <- situation
 
       # create the simulation with new parameters
-      id <- simplace::createSimulation(model_options$SimplaceInstance,param)
-      
+      id <- simplace::createSimulation(model_options$SimplaceInstance, param)
+
       # run the simulation
       simplace::runSimulations(model_options$SimplaceInstance)
-      
+
       # fetch the results and convert them to dataframe
-      res <- simplace::getResult(model_options$SimplaceInstance,model_options$OutputId,id)
+      res <- simplace::getResult(model_options$SimplaceInstance,
+                                 model_options$OutputId, id)
       df <- simplace::resultToDataframe(res)
-      
+
       # Add the date column as CroptimizeR requires it
-      # If required, some additional (post)processing can be performed here if 
+      # If required, some additional (post)processing can be performed here if
       # the Simplace output does not comply to the CroptimizeR standards
       df$Date <- df$CURRENT.DATE
-      
+
       # Add the result of the situation
-      results$sim_list[[situation]] = df
+      results$sim_list[[situation]] <- df
     }
-    
+
     # if every simulation succeeded, set error to FALSE
-    results$error = FALSE
+    results$error <- FALSE
   },
-  error = function(e){print(paste("Something went wrong in Simplace: ",e))}
+  error = function(e) {
+      print(paste("Something went wrong in Simplace: ", e))
+    }
   )
-  
+
   # return the results
   results
 }
--- R/CroptimizeR/runCroptimizer.R
+++ R/CroptimizeR/runCroptimizer.R
@@ -10,79 +10,85 @@ source("SimplaceFunction.R")
 model_function <- SimplaceFunction
 
 #' Give more memory to the java process
-options(java.parameters= "-Xmx6g")
+options(java.parameters = "-Xmx6g")
 
-#' Define the model options for Simplace 
+#' Define the model options for Simplace
 model_options <- within(list(), {
-  
-  InstallDir<- simplace::findFirstSimplaceInstallation() # Simplace java classes
-  WorkDir <- paste0(getwd(),'/simulation/') # solution and data files for the simulations
-  OutputDir <- paste0(getwd(),'/output/') # Simplace output directory
-  
-  SolutionFile <-paste0(WorkDir,"solution/YieldSlaLueRgrl.sol.xml") # Solution to run
+
+  InstallDir <- simplace::findFirstSimplaceInstallation() # Simplace java files
+  WorkDir <- paste0(getwd(), "/simulation/") # solution and data files
+  OutputDir <- paste0(getwd(), "/output/")   # Simplace output directory
+
+  SolutionFile <- paste0(WorkDir, "solution/YieldSlaLueRgrl.sol.xml") # Solution
 
   # Define situations - e. g. load them from a project data file
   ProjectDataFile <- paste0(WorkDir, "data/climatezones.csv")
-  ProjectData <- if(is.null(ProjectDataFile)){NULL}else{readr::read_delim(ProjectDataFile, delim=";")}
+  ProjectData <- if (is.null(ProjectDataFile)) {
+      NULL
+    } else {
+      readr::read_delim(ProjectDataFile, delim = ";")
+    }
   Situations <- unique(ProjectData$CLIMATEZONE)
-  
+
   # Supply the variable name that defines which situation will run
-  SituationVariable <- 'vCLIMATEZONE'
-  
+  SituationVariable <- "vCLIMATEZONE"
+
   # Id of MEMORY output that provides the simulation results
-  OutputId <- 'LintulSum'
+  OutputId <- "LintulSum"
 
   # Initialise Simplace framework and store the handle
   SimplaceInstance <- simplace::initSimplace(InstallDir, WorkDir, OutputDir)
-  
+
 })
 
 #' Set Check- and LogLevel and load the simulation
-simplace::setCheckLevel(model_options$SimplaceInstance,"OFF")
-simplace::openProject(model_options$SimplaceInstance, 
+simplace::setCheckLevel(model_options$SimplaceInstance, "OFF")
+simplace::openProject(model_options$SimplaceInstance,
                       model_options$SolutionFile)
 simplace::setLogLevel("WARN")
 
-#' 
+#'
 #' # Preparations for CroptimizeR
-#' 
+#'
 
 #' Read the observed data
-obs<- readr::read_delim("data/observations.csv",delim=";") 
-obs_list <- split(obs,f=obs$CLIMATEZONE)
+obs <- readr::read_delim("data/observations.csv", delim = ";")
+obs_list <- split(obs, f = obs$CLIMATEZONE)
 names(obs_list) <- model_options$Situations
 
 #' Define calibration parameters and their range
-param_info <- list(lb=c(vLUE=0,vSLA=0,vRGRL=0.00001),ub=c(vLUE=10,vSLA=0.1,vRGRL=0.05))
+param_info <- list(lb = c(vLUE =  0, vSLA = 0.0, vRGRL = 0.00001),
+                   ub = c(vLUE = 10, vSLA = 0.1, vRGRL = 0.05))
 
 #' Set the options for CroptimizeR
-optim_options=list(path_results = paste0(getwd(),'/output'),   # path where to store the results (graph and Rdata)
-                   nb_rep = 2,              # Number of repetitions of the minimization;
-                   ranseed = 1234,           # set random seed so that each execution give the same results
-                   # If you want randomization, don't set it.
-                   maxeval= 50)  
+optim_options <- list(path_results = paste0(getwd(), "/output"),   # path where
+                   # to store the results (graph and Rdata)
+                   nb_rep = 2,     # Number of repetitions of the minimization;
+                   ranseed = 1234, # set random seed so that each execution give
+                   # the same results. If you want randomization, don't set it.
+                   maxeval = 50)
 
-#' 
-#' # Runing the optimisation 
-#' 
+#'
+#' # Runing the optimisation
+#'
 
 #' Run the optimisation process by CroptimizeR
-CroptimizR::estim_param(obs_list=obs_list,
-                        model_function = model_function, 
-                        model_options = model_options, 
+CroptimizR::estim_param(obs_list = obs_list,
+                        model_function = model_function,
+                        model_options = model_options,
                         optim_options = optim_options,
-                        param_info=param_info )
+                        param_info = param_info)
 
-#' 
+#'
 #' # Cleanup
-#' 
+#'
 
 #' Close the simplace project (simulation)
 simplace::setLogLevel("INFO")
 simplace::closeProject(model_options$SimplaceInstance)
 
 #'
-#' # Analyze the optimisation result 
-#' 
+#' # Analyze the optimisation result
+#'
 
 #' ...
